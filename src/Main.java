2750ad3120fa2211cca98c32e46fd2bf4b5a69b0
fixss <fixssss@gmail.com>
2022-10-03 15:43:22 +0300

    Eraser Complete

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ import ru.netology.PersonSurnameComparator;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class Main {
     public static void main(String[] args) {
@@ -16,6 +17,9 @@ public class Main {
         people.add(new Person("Malfoy.Malfoy.Malfoy.Malfoy.Malfoy.Malfoy.Malfoy.Malfoy", "Draco", 15));
         people.add(new Person("Malfoy.Malfoy.Malfoy.Malfoy.Malfoy.Malfoy.Malfoy", "Lucius", 55));
         people.add(new Person("Lord.Lord.Lord.Lord.Lord.Lord.Lord.Lord.Lord", "voldemort", 99));
+
+        Predicate<Person> isBelow18 = p -> p.getAge() < 18;
+        people.removeIf(isBelow18);
 
         Collections.sort(people, new PersonSurnameComparator(6));
         System.out.println(people);
